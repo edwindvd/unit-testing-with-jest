353554a766d100d916045d1d6ff463dafa17d9f0
edwindvd <edwindavidcr7@gmail.com>
2022-06-20 19:46:34 +0000

    continuacion

--- .learn/resets/11-tests-skelleton/app.js
+++ .learn/resets/11-tests-skelleton/app.js
@@ -10,17 +10,11 @@ const fromDollarToYen = function(valueInDollar){
 return (valueInDollar / 1.29) * 127.9;
 // let valueInYen = valueInEuro * 127.9; 
 }
-// console.log(fromDollarToYen(15)); probando la función
-
-const fromYentoPound = function(valueInYen){
-    let valueInPound = (valueInYen / 127.9) * 0.8; 
-    return  valueInPound;
-}
-// console.log(fromYentoPound(1)); probando la función
+console.log(fromDollarToYen(15));
 
 const sum = (a,b) => {
     return a + b
 }
 
 console.log(sum(7,3))
-module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYentoPound };
+module.exports = { sum, fromEuroToDollar, fromDollarToYen };
--- test.js
+++ test.js
@@ -23,3 +23,10 @@ test(" result should be 1487.2093023255813 ", function(){
 
     expect(fromDollarToYen(15)).toBe(1487.2093023255813);
 })
+
+test(" result should be  ", function(){
+
+    const { fromYentoPound } = require('./app.js')
+
+    expect(fromYentoPound()).toBe();
+})
